d64401eae80df0d5fb5beb8a51176010d8abb5c9
Vitória Souza <vitoria.souza@acct.global>
2021-11-27 12:48:33 -0300

    added enum

--- data/db.js
+++ data/db.js
@@ -4,14 +4,16 @@ const users = [
     name: "Danilo Caraça",
     email: "danilo@teste.com",
     age: 30,
-    profileId: 1
+    profileId: 1,
+    status: "BLOCKED"
   },
   {
     id: 2,
     name: "Leila Souza",
     email: "leila@teste.com",
     age: 42,
-    profileId: 2
+    profileId: 2,
+    status: "ACTIVE"
   }
 ]
 
