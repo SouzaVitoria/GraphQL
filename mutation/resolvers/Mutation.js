101ffb9f943a8abffdbb7f88d586e982625e2d25
Vitória Souza <vitoria.souza@acct.global>
2021-11-29 16:03:08 -0300

    validation of existing email

--- mutation/resolvers/Mutation.js
+++ mutation/resolvers/Mutation.js
@@ -1,6 +1,12 @@
 const { users, nextId } = require('../data/db')
 
 function newUser(_, args) {
+  const emailCreated = users.some(user => user.email === args.email)
+
+  if (emailCreated) {
+    throw new Error("E-mail cadastrado")
+  }
+
   const newUser = {
     id: nextId(),
     ...args,
