10bdd4e2ea47eb17647e69d5aaae95fcd8b968a5
Vitória Souza <vitoria.souza@acct.global>
2021-11-30 16:15:26 -0300

    added input type

--- mutation/resolvers/Mutation.js
+++ mutation/resolvers/Mutation.js
@@ -1,7 +1,20 @@
 const { users, nextId } = require('../data/db')
 
-function newUser(_, args) {
-  const emailCreated = users.some(user => user.email === args.email)
+function indexUser(filter) {
+ if(!filter) return -1
+ const { id, email }= filter
+
+ if(id) {
+   return users.findIndex(user => user.id === id)
+ } else if (email) {
+   return users.findIndex(user => user.email === email)
+ }
+
+ return -1
+}
+
+function newUser(_, { data }) {
+  const emailCreated = users.some(user => user.email === data.email)
 
   if (emailCreated) {
     throw new Error("E-mail cadastrado")
@@ -9,7 +22,7 @@ function newUser(_, args) {
 
   const newUser = {
     id: nextId(),
-    ...args,
+    ...data,
     profileId: 1,
     status: 'ACTIVE'
   }
@@ -18,8 +31,8 @@ function newUser(_, args) {
   return newUser
 }
 
-function deleteUser (_, { id }) {
-  const i = users.findIndex(user => user.id === id)
+function deleteUser (_, { filter }) {
+  const i = indexUser(filter)
 
   if (i < 0) return null
 
@@ -28,14 +41,14 @@ function deleteUser (_, { id }) {
   return deleteUser ? deleteUser[0] : null
 }
 
-function updateUser (_, args) {
-  const i = users.findIndex(user => user.id === args.id)
+function updateUser(_, { filter, data }) {
+  const i = indexUser(filter)
 
   if (i < 0) return null
 
   const user = {
     ...users[i],
-    ...args
+    ...data
   }
 
   users.splice(i, 1, user)
