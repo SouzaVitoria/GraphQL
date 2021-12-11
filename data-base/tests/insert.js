b430e1dd26dff0d02ff0799a92e25a6de52ceba6
Vitória Souza <vitoria.souza@acct.global>
2021-12-11 13:41:07 -0300

    added super profile - insert data

--- data-base/tests/insert.js
+++ data-base/tests/insert.js
@@ -6,3 +6,10 @@ const newProfile = {
 }
 
 db('profiles').insert(newProfile).then(response => console.log(response)).catch(error => console.log(error.sqlMessage)).finally(() => db.destroy())
+
+const profileSU = {
+  name: 'root' + Math.floor(Math.random() * 10) + 1,
+  rotule: 'Super Usuário'
+}
+
+db.insert(profileSU).into('profiles').then(response => console.log(response)).catch(error => console.log(error.sqlMessage)).finally(() => db.destroy())
