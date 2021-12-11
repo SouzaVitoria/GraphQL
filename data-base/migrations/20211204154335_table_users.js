bd702f43cb9175cc61ea67792421466c3b835ab1
Vitória Souza <vitoria.souza@acct.global>
2021-12-11 09:12:51 -0300

    fix user table id field

--- data-base/migrations/20211204154335_table_users.js
+++ data-base/migrations/20211204154335_table_users.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable('users', table => {
-    table.string('id').primary();
+    table.increments('id').primary();
     table.string('name').notNull();
     table.string('email').notNull().unique();
     table.string('password', 60).notNull();
