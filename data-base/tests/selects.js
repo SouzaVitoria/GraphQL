ac1acbee5b336de5e9664b5a545cf572f0e97af8
Vitória Souza <vitoria.souza@acct.global>
2021-12-11 13:56:59 -0300

    select profiles - method two

--- data-base/tests/selects.js
+++ data-base/tests/selects.js
@@ -1,3 +1,5 @@
 const db = require('../config/db')
 
 db('profiles').map(profile => profile.name).then(names => console.log(names)).finally(() => db.destroy())
+
+db('profiles').select('name', 'id').then(response => console.log(response)).finally(() => db.destroy())
