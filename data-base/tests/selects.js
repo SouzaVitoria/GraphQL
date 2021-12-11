a0da75ee12f784efed34f0b2f50e890cfd9ab43a
Vitória Souza <vitoria.souza@acct.global>
2021-12-11 14:01:28 -0300

    select profiles - added position

--- data-base/tests/selects.js
+++ data-base/tests/selects.js
@@ -4,4 +4,4 @@ db('profiles').map(profile => profile.name).then(names => console.log(names)).fi
 
 db('profiles').select('name', 'id').then(response => console.log(response)).finally(() => db.destroy())
 
-db.select('name', 'id').from('profiles').limit(3).then(response => console.log(response)).finally(() => db.destroy())
+db.select('name', 'id').from('profiles').limit(3).offset(1).then(response => console.log(response)).finally(() => db.destroy())
