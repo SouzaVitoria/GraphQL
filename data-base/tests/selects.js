26eaf7b117e02f6190b73f9bc4ae415523e0d617
Vitória Souza <vitoria.souza@acct.global>
2021-12-11 13:51:44 -0300

    using map - select data

--- data-base/tests/selects.js
+++ data-base/tests/selects.js
@@ -1,3 +1,3 @@
 const db = require('../config/db')
 
-db('profiles').then(response => console.log(response)).finally(() => db.destroy())
+db('profiles').then(response => response.map(profile => profile.name)).then(names => console.log(names)).finally(() => db.destroy())
